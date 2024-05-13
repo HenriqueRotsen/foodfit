310ad97264535ef8a45b338350188abcec8f745d
Henrique Rotsen <henriquerotsensf@gmail.com>
2024-05-13 16:20:08 -0300

    Rota da pagina inicial

---
+++ client/src/routes/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import LoginPage from "../pages/loginPage";
+
+
+// websystem
+
+function RoutesApp() {
+
+  //const {user} = useContext(appContext)
+
+  return (
+    <LoginPage></LoginPage>
+  )
+}
+
+export default RoutesApp
